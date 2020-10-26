1f048435286ed2d6356a071db9725b8347c8640d
Sri Harsha Varma Uppalapati <harsha@Sris-MacBook-Air.local>
2020-10-26 13:43:48 -0400

    Updated authorisation tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,21 +3,6 @@ from flask import g, session
 from flaskr.db import get_db
 
 
-def test_register(client, app):
-    assert client.get('/auth/register').status_code == 200
-    response = client.post(
-        '/auth/register', data={'username': 'a', 'password': 'a'}
-    )
-    assert 'http://localhost/auth/login' == response.headers['Location']
-
-    with app.app_context():
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute(
-            "select * from user where username = 'a'",
-        )
-        assert cursor.fetchone() is not None
-
 @pytest.mark.parametrize(('username', 'password', 'message'), (
     ('a', 'test', b'Incorrect username.'),
     ('test', 'a', b'Incorrect password.'),
