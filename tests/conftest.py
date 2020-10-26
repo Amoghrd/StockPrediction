0f4f250ac3302b6878ee4ecfd68ec5261d5da9f2
Sri Harsha Varma Uppalapati <harsha@Sris-MacBook-Air.local>
2020-10-25 21:45:16 -0400

    Migrated tests from sqlite to mysql

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,6 @@ import pytest
 from flaskr import create_app
 from flaskr.db import get_db, init_db
 
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
-
-
 @pytest.fixture
 def app():
     db_fd, db_path = tempfile.mkstemp()
@@ -20,7 +16,10 @@ def app():
 
     with app.app_context():
         init_db()
-        get_db().executescript(_data_sql)
+        db = get_db()
+        cursor = db.cursor()
+        with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+            cursor.execute(f.read().decode('utf-8'), multi=True)
 
     yield app
 
